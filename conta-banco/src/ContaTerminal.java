ff5dae99599010f6046d505bb8d15059fecb2336
anaualima <alissandralima@gmail.com>
2024-08-06 11:23:21 -0300

    feat: todo about challenge

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:Conhecer e importar a classe scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir mensagem conta criada
     }
 }
